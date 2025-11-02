d2199cfc4dfc30a45c3e030dc597475d020ea566
nikhilpj <nikhilpj98@gmail.com>
2025-11-02 20:39:24 +0530

    Add project files.

---
+++ api/Models/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Stock
+{
+	public int Id { get; set; }
+	public string Symbol { get; set; } = string.Empty;
+	public string CompanyName { get; set; } = string.Empty;
+	[Column(TypeName = "decimal(18,2)")]
+	public decimal Purchase { get; set; }
+	[Column(TypeName = "decimal(18,2)")]
+	public decimal LastDiv { get; set; }
+	public string Industry { get; set; } = string.Empty;
+	public long MarketCap { get; set; }
+	public List<Comment> Comments = new List<Comment>();
+}
