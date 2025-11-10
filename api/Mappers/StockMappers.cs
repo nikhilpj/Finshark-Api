89e75e2bad3562946b31931ba8acbe35e4fa352b
nikhilpj <nikhilpj98@gmail.com>
2025-11-10 23:07:02 +0530

    Configured Dtos and Mappers

---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,21 @@
+﻿using api.Dtos.Stock;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock StockModel)
+        {
+            return new StockDto
+            {
+                Id = StockModel.Id,
+                Symbol = StockModel.Symbol,
+                CompanyName = StockModel.CompanyName,
+                Purchase = StockModel.Purchase,
+                LastDiv = StockModel.LastDiv,
+                Industry = StockModel.Industry,
+                MarketCap = StockModel.MarketCap
+            };
+        }
+    }
+}
