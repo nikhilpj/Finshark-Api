f7349717becbd9f3074d51b6fb15625c58ff05b5
nikhilpj <nikhilpj98@gmail.com>
2025-11-16 22:44:43 +0530

    endpoint to add stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,4 +1,5 @@
 ﻿using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +34,15 @@ namespace api.Controllers
 
             }
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto )
+        {
+            var stockModel = stockDto.ToStockFromCreateDto();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
     }
 }
--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -17,5 +17,18 @@ namespace api.Mappers
                 MarketCap = StockModel.MarketCap
             };
         }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap= stockDto.MarketCap
+            };
+        }
     }
 }
