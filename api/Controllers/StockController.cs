4256e88f5ea691a5826f4c11430037d63478f261
nikhilpj <nikhilpj98@gmail.com>
2025-12-08 11:53:55 +0530

    update stock endpoint configured

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -44,5 +44,25 @@ namespace api.Controllers
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
+        }
     }
 }
